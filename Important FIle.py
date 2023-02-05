89bf6ec1b5b7ce81e5d0fc4e4a83bc9264e1e0d3
sunil1510k <87171083+sunil1510k@users.noreply.github.com>
2023-02-05 09:41:17 +0530

    Add files via upload

---
+++ Important FIle.py
@@ -0,0 +1,36 @@
+
+"""
+f = open("data.txt", "w")
+f.write('This is the First Line')
+f.close()
+"""
+
+"""
+f = open("data.txt", "w")
+f.write('This is the Second Line')
+f.close()
+"""
+
+
+f = open("data.txt", "a")
+f.write('\n\n\n. Task Done')
+f.close()
+
+
+
+"""
+f = open("NewFile.txt", "a")
+f.write('This is the First Line with Append')
+f.close()
+"""
+
+"""
+f = open("NewFile.txt", "a")
+f.write('Ending with Next Line')
+f.close()
+"""
+
+
+
+
+
